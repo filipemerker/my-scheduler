50edfa9e60ec3c18173fbedc14ff96fa619d34b9
Filipe Merker <filipesmerker@gmail.com>
2025-11-30 18:22:43 -0300

    feat: Add vecel data

--- client/src/apolloClient.ts
+++ client/src/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:4000/graphql',
+  uri: import.meta.env.VITE_GRAPHQL_URL || 'http://localhost:4000/graphql',
 });
 
 const client = new ApolloClient({
